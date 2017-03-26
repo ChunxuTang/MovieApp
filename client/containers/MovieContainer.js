121810db39061e46ee23aaab75235974fcb37aea
JoshuaTang <1240604020@qq.com>
2017-03-25 21:05:47 -0400

    add support for mobile device

--- client/containers/MovieContainer.js
+++ client/containers/MovieContainer.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchMovies } from '../actions/index';
+import axios from 'axios';
 
 import MovieList from './MovieList';
 import MovieDetail from './MovieDetail';
 
 class MovieContainer extends Component {
   componentDidMount() {
-    // axios.get('/movies')
-    //   .then((res) => {
-    //     this.props.fetchMovies(res.data);
-    //   });
+    axios.get('/movies')
+      .then((res) => {
+        this.props.fetchMovies(res.data);
+      });
 
-    fetch('/movies')
-      .then(res => res.json())
-      .then(data => this.props.fetchMovies(data))
-      .catch(err => console.log(err));
+    // fetch('/movies')
+    //   .then(res => res.json())
+    //   .then(data => this.props.fetchMovies(data))
+    //   .catch(err => console.log(err));
   }
 
   render() {
